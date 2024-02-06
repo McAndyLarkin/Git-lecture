6511bf3abea0b2f4e69c18967326004521d5aebf
McAndyLarkin <51488300+McAndyLarkin@users.noreply.github.com>
2024-02-06 13:27:29 +0300

    Feature 6 (#5)
    * Feature 6: [f5] + 4 + 7
    
    ---------
    
    Co-authored-by: Makssokolov <makssokolov@yandex-team.ru>

--- feature1/Code.kt
+++ feature1/Code.kt
@@ -7,11 +7,12 @@ class Activity {
     }
 
     private fun initUI() {
+        1 + 2+ 3 + 4
         initText()
         initEditText()
         initSpinner()
         initButtons()
-        1 + 5
+        1 + 5 + 4 + 7
     }
 
     private fun initFirebase() = Unit
