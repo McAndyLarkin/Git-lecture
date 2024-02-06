351b4efbe1fd8c5b97a46adf4266a9a17c3f26f3
Makssokolov <makssokolov@yandex-team.ru>
2024-02-06 12:23:57 +0300

    Feature1 UI+Firebase

---
+++ feature1/Code.kt
@@ -0,0 +1,11 @@
+class Activity {
+    fun onCreate() {
+//        super().onCreate()
+        initUI()
+        initFirebase()
+    }
+
+    private fun initUI() = Unit
+
+    private fun initFirebase() = Unit
+}
